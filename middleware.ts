89be41c775505cb0d151610562cbe185eab95b02
AMADRO <asd@example.com>
2024-11-13 09:32:53 +0100

    add middleware for authorization

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
+export async function middleware(request: NextRequest) {
+    const { isAuthenticated } = getKindeServerSession();
+    const isUserAuthenticated = await isAuthenticated();
+
+    if (!isUserAuthenticated) {
+        return NextResponse.redirect(new URL('/', request.url));
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/dashboard/:path*'],
+};
