97b25f6d31625e1eeb593cb13053a2b447d4178f
AMADRO <asd@example.com>
2024-11-13 12:08:22 +0100

    fill whole screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex flex-col">
+        <div className="flex flex-col h-[100vh]">
           <NavBar />
           {children}
         </div>
