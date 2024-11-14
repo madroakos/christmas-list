a5d2dafa0f06e2c2ce0c8c8b81f3d03d6c775bf7
AMADRO <asd@example.com>
2024-11-14 15:00:51 +0100

    add mypage path

--- middleware.ts
+++ middleware.ts
@@ -17,5 +17,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/', '/dashboard/:path*'],
+    matcher: ['/', '/dashboard/:path*', '/mypage/:path*'],
 };
