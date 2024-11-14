2352571682ab67ac7d35543430861223d2863cb5
AMADRO <asd@example.com>
2024-11-14 14:40:01 +0100

    refactor DashboardPage to improve layout and handle empty item state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,36 +6,32 @@ import { formatPrice } from "@/app/helpers/formatPrice";
 export default async function DashboardPage() {
     const items = await getData();
 
-    if (items) {
-        return (
-            <div className="flex flex-col gap-6 p-12">
-                {items.map((userWithItems) => (
-                    <div key={userWithItems.user.id}>
-                        <Link href={`/user/${userWithItems.user.userID}`} passHref>
-                            <h1 className="font-bold text-2xl mb-3">{userWithItems.user.given_name} {userWithItems.user.family_name}</h1>
-                        </Link>
-                        <div className="flex flex-col gap-6">
-                            {userWithItems.items.map((item) => (
-                                <Link key={item.id} href={item.link} passHref>
-                                    <div className="flex flex-row gap-3">
-                                        <Image width={80} height={80} src={item.photoLink} alt={item.name} className="rounded-lg" />
-                                        <div className="flex flex-col self-center">
-                                            <h2 className="font-bold text-lg">{item.name}</h2>
-                                            <p>{formatPrice(item.price)}</p>
-                                        </div>
+    return (
+        <div className="flex flex-col gap-6 p-12">
+            <h1 className="font-bold text-3xl self-center">Santa&apos;s list</h1>
+
+            {items ? items.map((userWithItems) => (
+                <div key={userWithItems.user.id}>
+                    <Link href={`/user/${userWithItems.user.userID}`} passHref>
+                        <h1 className="font-bold text-2xl mb-3">{userWithItems.user.given_name} {userWithItems.user.family_name}</h1>
+                    </Link>
+                    <div className="flex flex-col gap-6">
+                        {userWithItems.items.map((item) => (
+                            <Link key={item.id} href={item.link} passHref>
+                                <div className="flex flex-row gap-3">
+                                    <Image width={80} height={80} src={item.photoLink} alt={item.name} className="rounded-lg" />
+                                    <div className="flex flex-col self-center">
+                                        <h2 className="font-bold text-lg">{item.name}</h2>
+                                        <p>{formatPrice(item.price)}</p>
                                     </div>
-                                </Link>
-                            ))}
-                        </div>
+                                </div>
+                            </Link>
+                        ))}
                     </div>
-                ))}
-            </div>
-        )
-    } else {
-        return (
-            <div className="flex self-center">
+                </div>
+            )) : (<div className="flex self-center">
                 <h1 className="font-bold text-2xl">No items to display</h1>
-            </div>
-        );
-    }
+            </div>)}
+        </div>
+    )
 }
