3c22895e9462a87e2fa9dfa0994c4c187929d65c
AMADRO <asd@example.com>
2024-11-14 15:01:10 +0100

    enhance Home page with welcoming message and styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 export default async function Home() {
 
   return (
-    <div>
+    <div className="flex flex-col items-center h-full pt-12">
+      <p>Welcome to the <span className="text-xl font-bold bg-gradient-to-r from-red-600 via-green-600 to-white bg-clip-text text-transparent mb-3">Public Wishlist</span> app!</p>
+      <p className="font-bold">Share your inner most desire with your loved ones</p>
     </div>
   );
 }
