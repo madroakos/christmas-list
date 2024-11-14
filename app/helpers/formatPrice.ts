b957cd3a8147b4c7c16ccdd1f2d768540f408da9
AMADRO <asd@example.com>
2024-11-14 12:51:35 +0100

    add formatPrice helper function for currency formatting

---
+++ app/helpers/formatPrice.ts
@@ -0,0 +1,7 @@
+export function formatPrice(price: number) {
+    return new Intl.NumberFormat('hu-HU', {
+        style: 'currency',
+        currency: 'HUF',
+        maximumFractionDigits: 0
+    }).format(price);
+};
