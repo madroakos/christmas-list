1cc3e683c07306536d984e39bd68f44c7fcc94ca
AMADRO <asd@example.com>
2024-11-14 10:43:37 +0100

    add functions to retrieve user by ID, followed users, and items by user

--- prisma/actions.ts
+++ prisma/actions.ts
@@ -1,10 +1,42 @@
+import { User } from "@prisma/client";
 import prisma from "./db";
+
+export async function getUserById(userId: string) {
+    'use server';
+    return prisma.user.findUnique({
+        where: {
+            userID: userId,
+        },
+    });
+}
 
 export async function deleteItem(itemId: number) {
     'use server';
     return prisma.wishlistItem.delete({
         where: {
             id: itemId,
+        },
+    });
+}
+
+export async function getFollowedUsers(follower: User) {
+    'use server';
+    return prisma.user.findMany({
+        where: {
+            followers: {
+                some: {
+                    follower: follower,
+                },
+            },
+        },
+    });
+}
+
+export async function getItemsByUser(userId: number) {
+    'use server';
+    return prisma.wishlistItem.findMany({
+        where: {
+            userId,
         },
     });
 }
