862edf33ce7b9113de41f0bf7a8e62a5b943833b
AMADRO <asd@example.com>
2024-11-15 16:19:26 +0100

    Start implementing user follow functionality, not finished

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -3,31 +3,57 @@ import Link from "next/link"
 import Image from "next/image"
 import { formatPrice } from "@/app/helpers/formatPrice"
 import UserAvatar from "@/app/components/UserAvatar"
+import { getIfUserFollows } from "@/prisma/actions"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
-    const userId = (await params).id
+    const userId = (await params).id;
+    const { isAuthenticated, getUser } = getKindeServerSession();
+    const isUserAuthenticated = await isAuthenticated();
+    let currentUser = null;
+    let currentUserDBId = null;
+    if (isUserAuthenticated) {
+        currentUser = await getUser();
+        currentUserDBId = await prisma.user.findFirst({
+            where: {
+                userID: userId
+            }
+        })
+    }
 
-    const user = await prisma.user.findUnique({
+    const userSearchedFor = await prisma.user.findUnique({
         where: {
             userID: userId
         }
     })
 
+    if (!userSearchedFor) {
+        return <div>User not found</div>
+    }
+
     const items = await prisma.wishlistItem.findMany({
         where: {
-            ownerUserId: user?.id
+            ownerUserId: userSearchedFor?.id
         }
     })
 
-    if (!user) {
-        return <div>User not found</div>
-    }
+    let isFollowedAlready = null;
+    if (currentUserDBId) isFollowedAlready = await getIfUserFollows(currentUserDBId.id, userSearchedFor.id)
+
 
     return (
         <div className="flex flex-col items-center px-12">
             <div className="flex flex-col items-center gap-3">
-                <UserAvatar user={user} />
-                <div className="text-2xl font-bold">{user.given_name} {user.family_name}</div>
+                <UserAvatar user={userSearchedFor} />
+                <div className="text-2xl font-bold">{userSearchedFor.given_name} {userSearchedFor.family_name}</div>
+                {isUserAuthenticated && !isFollowedAlready && (
+                    <form action="/api/follow" method="post">
+                        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-24 self-center text-center">Follow</button>
+                    </form>)}
+                {isUserAuthenticated && isFollowedAlready && (
+                    <form action="/api/logout" method="post">
+                        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-24 self-center text-center">Follow</button>
+                    </form>)}
             </div>
             <div className="flex flex-col gap-3 mt-6 w-full">
                 {items.map(item => {
