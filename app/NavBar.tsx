fe0444187950fa4ca6fba23c445e1c317e90a3cc
AMADRO <asd@example.com>
2024-11-15 11:25:05 +0100

    Refactor NavBar component for improved layout and responsiveness

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,40 +6,72 @@ export default async function NavBar() {
     const isUserAuthenticated = await isAuthenticated();
 
     return (
-        <nav className='h-12 flex flex-row justify-between items-center px-6 py-12'>
-            <div className="">
-                <Link href='/'>
-                    <span className="text-4xl font-bold bg-gradient-to-r from-red-600 via-green-600 to-white bg-clip-text text-transparent">Public Wishlist</span>
-                </Link>
-            </div>
-            <div>
-                <ul className='flex flex-row gap-3'>
-                    {isUserAuthenticated ? (
-                        <>
-                            <Link href='/mypage'>
-                                <li className="btn btn-primary">My page</li>
-                            </Link>
-                            <LogoutLink>
-                                <li className="btn btn-primary">Sign out</li>
-                            </LogoutLink>
+        <>
+            < div className="navbar p-3">
+                <div className="flex-1">
+                    <Link href={"/"} className="btn btn-ghost text-xl sm:text-2xl md:text-4xl bg-gradient-to-r from-red-600 via-green-600 to-white bg-clip-text text-transparent">Wishy</Link>
+                </div>
+                <div className="flex-none">
+                    <div className={` ${isUserAuthenticated ? 'dropdown dropdown-end' : ''} md:hidden`}>
+                        {isUserAuthenticated ? (
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 sm:w-64 rounded-full">
+                                    <img
+                                        alt="Tailwind CSS Navbar component"
+                                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                                </div>
+                            </div>
+                        ) : (
+                            <LoginLink>Login</LoginLink>
+                        )}
+                        {isUserAuthenticated ? (
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-40 p-2 shadow">
 
-                        </>
-                    ) : (
-                        <>
-                            <LoginLink>
-                                <li className="btn btn-primary">
-                                    Sign in
-                                </li>
-                            </LoginLink>
-                            <RegisterLink>
-                                <li className="btn btn-primary">
-                                    Sign up
-                                </li>
-                            </RegisterLink>
-                        </>
-                    )}
-                </ul>
-            </div>
-        </nav>
+                                <>
+                                    <li>
+                                        <Link href={"/"} className="justify-between">
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href={"/mypage"} className="justify-between">
+                                            Profile
+                                        </Link>
+                                    </li>
+                                    <li><LogoutLink>Logout</LogoutLink></li>
+                                </>
+
+                            </ul>
+                        ) : (<></>)}
+                    </div>
+                    <div className="hidden md:inline-block">
+                        <ul className="menu menu-horizontal text-xl">
+                            {isUserAuthenticated ? (
+                                <>
+                                    <li>
+                                        <Link href={"/"} className="justify-between">
+                                            Home
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href={"/mypage"} className="justify-between">
+                                            Profile
+                                        </Link>
+                                    </li>
+                                    <li><LogoutLink>Logout</LogoutLink></li>
+                                </>
+                            ) : (
+                                <>
+                                    <li><LoginLink>Sign in</LoginLink></li>
+                                    <li><RegisterLink>Register</RegisterLink></li>
+                                </>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </div >
+        </>
     );
 }
