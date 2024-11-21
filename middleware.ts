b9bb5e6c7c92cb12f00a4fef38e5b24e6b58e698
madroakos <madroboy@protonmail.com>
2024-11-21 20:57:35 +0100

    for some reason adding the console.log fixes it

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,30 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 export async function middleware(request: NextRequest) {
-    const { isAuthenticated, getUser } = getKindeServerSession();
-    const isUserAuthenticated = await isAuthenticated();
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  console.log("isUserAuthenticated", isUserAuthenticated);
 
-    if (!isUserAuthenticated && request.nextUrl.pathname !== '/') {
-        return NextResponse.redirect(new URL('/', request.url));
+  if (!isUserAuthenticated && request.nextUrl.pathname !== "/") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (isUserAuthenticated && request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (isUserAuthenticated) {
+    const user = await getUser();
+    if (request.nextUrl.pathname === `/user/${user.id}`) {
+      return NextResponse.redirect(new URL("/mypage", request.url));
     }
+  }
 
-    if (isUserAuthenticated && request.nextUrl.pathname === '/') {
-        return NextResponse.redirect(new URL('/dashboard', request.url));
-    }
-
-    if (isUserAuthenticated) {
-        const user = await getUser();
-        if (request.nextUrl.pathname === `/user/${user.id}`) {
-            return NextResponse.redirect(new URL('/mypage', request.url));
-        }
-    }
-
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: ['/', '/dashboard/:path*', '/mypage/:path*', '/user/:path*'],
+  matcher: ["/", "/dashboard/:path*", "/mypage/:path*", "/user/:path*"],
 };
